0aae064db86803ed3eb83905629fa6703e04bf83
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-26 16:36:43 +0300

    8.7 added editAuthor mutation

--- library-backend/index.js
+++ library-backend/index.js
@@ -114,6 +114,10 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -145,6 +149,15 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      let author = authors.find(author => author.name === args.name)
+      if (author) {
+        author = { ...author, born: args.setBornTo }
+        authors.map(a => a.name === author.name ? author : a)
+        return author
+      }
+      return null
     }
   }
 }
